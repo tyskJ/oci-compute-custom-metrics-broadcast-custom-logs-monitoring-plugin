ccd666aba886e919a5a3c49922ceb7cb82e87b18
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-24 00:19:27 +0900

    :memo: ...

--- envs/compartments.tf
+++ envs/compartments.tf
@@ -3,7 +3,7 @@ Compartment - workload
 ************************************************************/
 resource "oci_identity_compartment" "workload" {
   compartment_id = var.tenancy_ocid
-  name           = "oci-compute-custom-metrics-using-custom-logs-monitoring-plugin"
-  description    = "For OCI Compute Custom Metrics Using Custom Logs Monitoring Plugin"
+  name           = "oci-compute-custom-metrics-broadcast-custom-logs-monitoring-plugin"
+  description    = "For OCI Compute Custom Metrics Broadcast Custom Logs Monitoring Plugin"
   enable_delete  = true
 }
