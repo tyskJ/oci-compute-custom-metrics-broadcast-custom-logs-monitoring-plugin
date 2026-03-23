d2de738e663d96180eb102bdc7276ec07c58b6f2
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-23 13:22:44 +0900

    :recycle: 定義済みタグ追加

---
+++ envs/locals.tf
@@ -0,0 +1,9 @@
+/************************************************************
+Common
+************************************************************/
+locals {
+  common_defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag.key_env.name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag.key_managedbyterraform.name) = "true"
+  }
+}
