e59021dafdea2db6c61f90b187b7fc4cea433a3c
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-25 20:00:47 +0900

    :recycle: Windows Server 用エージェント構成追加

--- envs/monitoring.tf
+++ envs/monitoring.tf
@@ -1,6 +1,7 @@
 /************************************************************
 Agent Configurations
 ************************************************************/
+### Linux
 resource "oci_logging_unified_agent_configuration" "oracle" {
   is_enabled     = true
   compartment_id = oci_identity_compartment.workload.id
@@ -34,6 +35,49 @@ resource "oci_logging_unified_agent_configuration" "oracle" {
           "node_memory_SwapFree_bytes",
           "node_filesystem_free_bytes",
           "node_systemd_unit_state"
+        ]
+        # # 空で書いてもすべてにマッチする条件が書かれた<exclude>セクションが作成されてしまうため注意
+        # deny_list   = []
+      }
+    }
+  }
+  defined_tags = local.common_defined_tags
+}
+
+### Windows
+resource "oci_logging_unified_agent_configuration" "windows" {
+  is_enabled     = true
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "windowsserver-config"
+  description    = "For Windows Server Instance"
+  group_association {
+    group_list = [
+      oci_identity_dynamic_group.compute_windows.id
+    ]
+  }
+  service_configuration {
+    configuration_type = "MONITORING"
+    application_configurations {
+      source_type = "URL"
+      source {
+        name = "url-source"
+        scrape_targets {
+          name = "prometheus-windows-exporter"
+          url  = "http://localhost:9182/metrics"
+        }
+      }
+      destination {
+        compartment_id    = oci_identity_compartment.workload.id
+        metrics_namespace = "custom_metrics_namespace_windowsserver"
+      }
+
+      unified_agent_configuration_filter {
+        filter_type = "URL_FILTER"
+        name        = "windows-exporter-metrics-filter-regex"
+        allow_list = [
+          "windows_memory_available_bytes",
+          "windows_logical_disk_free_bytes",
+          "windows_service_state"
         ]
         # # 空で書いてもすべてにマッチする条件が書かれた<exclude>セクションが作成されてしまうため注意
         # deny_list   = []
